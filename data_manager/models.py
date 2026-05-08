305199b4509fcd987b51a2108afeee0a379466f6
Lena Voytek <lena.voytek@canonical.com>
2026-05-08 15:02:29 +0200

    Add new app for sending and receiving data

---
+++ data_manager/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class MergeBugPackageInfo(models.Model):
+    """Information about a package for creating a merges schedule merge bug."""
+
+    # Name of package - primary key
+    package = models.CharField(primary_key=True, max_length=200)
+
+    # When creating a new merge board, offset the expected milestone by this many months.
+    milestone_offset = models.IntegerField(default=0)
+
+
+class BackportBugPackageInfo(models.Model):
+    """Information about a package or package group for creating a backport bug."""
+
+    # Name of the package group - primary key
+    name = models.CharField(primary_key=True, max_length=200)
+
+    # Names of packages separated by commas
+    package_names_combined = models.CharField(primary_key=True, max_length=600)
+
+    # When creating a new merge board, offset the expected milestone by this many months.
+    milestone_offset = models.IntegerField(default=0)
+
+    # Description template for the bug
+    description_template = models.TextField(default="")
+
+    @property
+    def packages(self) -> list[str]:
+        """Return the list of package names."""
+        return [name.strip() for name in self.package_names_combined.split(",")]
+
+
