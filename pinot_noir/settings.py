8e31f27b019b7b20d3bb7c17d09bc00601cdc9c3
Lena Voytek <lena.voytek@canonical.com>
2026-05-12 23:44:07 +0200

    Update app inclusions

--- pinot_noir/settings.py
+++ pinot_noir/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Local apps
+    # Pinot Noir base applications
+    "pinot_noir.launchpad",
+    "pinot_noir.data_manager",
+    # Pinot Noir web page applications
     "pinot_noir.merges_schedule",
     "pinot_noir.reviews",
-    "pinot_noir.launchpad",
 ]
 
 MIDDLEWARE = [
