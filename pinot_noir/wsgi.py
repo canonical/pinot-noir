0d86cfad8960454776fd3258ca2768a620398407
Lena Voytek <lena.voytek@canonical.com>
2026-05-26 12:36:00 -0400

    Default to production settings for wsgi

--- pinot_noir/wsgi.py
+++ pinot_noir/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pinot_noir.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pinot_noir.settings.production")
 
 application = get_wsgi_application()
